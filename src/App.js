5930822fd19318d6d0bf4a41ce725d144630cc1c
arun <arunelavarasan168@gmail.com>
2024-07-10 20:44:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route, Router, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPages from "./pages/AboutPages";
+import ArticlePages from "./pages/ArticlePages";
+import ArticleListPages from "./pages/AricleListPages";
+import NavBar from './pages/NavBar';
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar/>
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPages />} />
+            <Route path="/articles" element={<ArticleListPages />} />
+            <Route path="/articles/:articleId" element={<ArticlePages />} />
+            <Route path="*" element={<NotFoundPage/>}/>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/AboutPages.js
@@ -0,0 +1,7 @@
+const AboutPages = () => {
+    return (
+        <h1>This is AboutPages page!</h1>
+    );
+}
+
+export default AboutPages;
